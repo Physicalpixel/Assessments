f8eec3026373c0e677f51d3f9062ba5712b628b9
Physicalpixel <33539074+Physicalpixel@users.noreply.github.com>
2021-08-02 14:38:46 +0530

    Update script.js

--- Assessment_2/script.js
+++ Assessment_2/script.js
@@ -17,7 +17,7 @@ svg1.append("text")
 
 
 let promises = [
-    d3.csv("https://cdn.jsdelivr.net/gh/Physicalpixel/Stock_Data/data_bind.csv")
+    d3.csv("https://raw.githubusercontent.com/Physicalpixel/Stock_Data/main/data_bind.csv")
 ]
 
 Promise.all(promises).then(ready)
